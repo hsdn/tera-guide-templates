c9de196b21aec9cd7b3a251824547ca6e2e9cc97
Dmitry Shin <dmitry.s@hsdn.org>
2020-11-30 11:00:12 +0400

    GF v100.2

---
+++ guides_translated/3204.js
@@ -0,0 +1,44 @@
+// Lumikan (High)
+
+
+module.exports = (dispatch, handlers, guide, lang) => {
+	guide.type = SP;
+
+	return {
+		"s-3204-1000-1101-0": [{ type: "text", sub_type: "message", message: "L mode front and rear base Attack 1 (atk01 | start)" }],
+		"s-3204-1000-1102-0": [{ type: "text", sub_type: "message", message: "L mode front and rear base Attack 1 (atk01 / End)" }],
+		"s-3204-1000-1103-0": [{ type: "text", sub_type: "message", message: "L mode front and rear base Attack 2 (atk01)" }],
+		"s-3204-1000-1104-0": [{ type: "text", sub_type: "message", message: "L mode forward base attack and then pull (atk02 / noloop)" }],
+		"s-3204-1000-1105-0": [{ type: "text", sub_type: "message", message: "L mode forward base attack and then pull (atk02 / loop)" }],
+		"s-3204-1000-1106-0": [{ type: "text", sub_type: "message", message: "L mode forward base attack 1st push (atk03 / noloop)" }],
+		"s-3204-1000-1107-0": [{ type: "text", sub_type: "message", message: "L mode forward base Attack 2 pull (atk03 / loop)" }],
+		"s-3204-1000-1108-0": [{ type: "text", sub_type: "message", message: "L mode forward base Attack source at distance (atk04 / noloop / end01)" }],
+		"s-3204-1000-1109-0": [{ type: "text", sub_type: "message", message: "L mode forward base Attack source at distance (atk04 / loop / end01)" }],
+		"s-3204-1000-1110-0": [{ type: "text", sub_type: "message", message: "L-mode the front and rear base attack ranged near-destruct after some pie (atk04 | noloop | end02)" }],
+		"s-3204-1000-1111-0": [{ type: "text", sub_type: "message", message: "L-mode the front and rear base attack ranged near-destruct after some pie (atk04 | loop | end02)" }],
+		"s-3204-1000-1112-0": [{ type: "text", sub_type: "message", message: "S-Mode range full-air personal cabinet and lucmia Center burst (atk05 / End02)" }],
+		"s-3204-1000-1113-0": [{ type: "text", sub_type: "message", message: "S-Mode range full-air personal sheet and lucmia Center burst (none)" }],
+		"s-3204-1000-1114-0": [{ type: "text", sub_type: "message", message: "S Mode range full attack frontal survival zone excluding one-width (atk05 / End03)" }],
+		"s-3204-1000-1115-0": [{ type: "text", sub_type: "message", message: "S-Mode forward base attack thrust and tear (atk06)" }],
+		"s-3204-1000-1116-0": [{ type: "text", sub_type: "message", message: "Straight-line attack (atk07)" }],
+		"s-3204-1000-1117-0": [{ type: "text", sub_type: "message", message: "S Mode forward base attack (atk08)" }],
+		"s-3204-1000-1118-0": [{ type: "text", sub_type: "message", message: "S Mode attacks 320 degrees to the rear and rotates 180 degrees (BackAtk02)" }],
+		"s-3204-1000-1119-0": [{ type: "text", sub_type: "message", message: "S Mode forward attack after 2 consecutive type (HeavyAtk | End01)" }],
+		"s-3204-1000-1120-0": [{ type: "text", sub_type: "message", message: "Range attack after two consecutive attacks (HeavyAtk | End02)" }],
+		"s-3204-1000-1121-0": [{ type: "text", sub_type: "message", message: "Create an inverse of S Mode Time (longAtk01 / noloop)" }],
+		"s-3204-1000-1122-0": [{ type: "text", sub_type: "message", message: "The S mode of the time force field generation (longAtk01 | loop)" }],
+		"s-3204-1000-1123-0": [{ type: "text", sub_type: "message", message: "S Mode forward spin attack (moveAtk01)" }],
+		"s-3204-1000-1124-0": [{ type: "text", sub_type: "message", message: "S mode, the reverse rotation attack (-moveAtk01)" }],
+		"s-3204-1000-1125-0": [{ type: "text", sub_type: "message", message: "S Mode forward random alter ego attack (reactionAtk)" }],
+		"s-3204-1000-1126-0": [{ type: "text", sub_type: "message", message: "S Mode forward random alter ego attack (- reactionAtk)" }],
+		"s-3204-1000-1127-0": [{ type: "text", sub_type: "message", message: "L mode counter-clockwise 270 degree attack (RoundAtkL01)" }],
+		"s-3204-1000-1128-0": [{ type: "text", sub_type: "message", message: "L mode Clockwise 270 degree attack (RoundAtkL02)" }],
+		"s-3204-1000-1129-0": [{ type: "text", sub_type: "message", message: "S mode only-rear rotation attack (UltraAtk)" }],
+		"s-3204-1000-1130-0": [{ type: "text", sub_type: "message", message: "S after the mode-forward rotation attack (-UltraAtk)" }],
+		"s-3204-1000-1131-0": [{ type: "text", sub_type: "message", message: "The S Mode and spin attack (UltraAtk02)" }],
+		"s-3204-1000-1150-0": [{ type: "text", sub_type: "message", message: "S Mode hangs" }],
+		"s-3204-1000-1151-0": [{ type: "text", sub_type: "message", message: "L mode hangs" }],
+		"s-3204-1000-1200-0": [{ type: "text", sub_type: "message", message: "Change to S Mode" }],
+		"s-3204-1000-1201-0": [{ type: "text", sub_type: "message", message: "Change to L mode" }]
+	};
+};
